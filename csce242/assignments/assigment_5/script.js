e58af313af4d3021e84ac4d79432a293aea6e2c7
DrewEvans0428 <aje5@email.sc.edu>
2024-09-15 20:21:38 -0400

    added assignment 5

--- csce242/assignments/assigment_5/script.js
+++ csce242/assignments/assigment_5/script.js
@@ -6,9 +6,6 @@ random.onclick = () => {
     location.reload();
 }
 
-/*function refreshPage(){
-    window.location.reload();
-}*/
 
 document.addEventListener('DOMContentLoaded', () => {
     const section = document.getElementById("count");
@@ -20,3 +17,19 @@ document.addEventListener('DOMContentLoaded', () => {
         clickCount.textContent = count;
     });
 });
+
+const slider = document.getElementById("myRange");
+const image = document.getElementById("slider-image");
+const container = document.querySelector(".three");
+
+slider.oninput = function() {
+
+    const containerWidth = container.offsetWidth;
+    const imageWidth = image.offsetWidth;
+    
+    const maxMove = containerWidth - imageWidth;
+
+    const leftPosition = ((this.value / 100) * 2 -1) * maxMove / 2;
+    image.style.transform = `translateX(${leftPosition}px)`;
+}
+
