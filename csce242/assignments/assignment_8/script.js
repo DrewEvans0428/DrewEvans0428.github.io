bb41917f350c91d0d046b30aadbef766b2adca8b
DrewEvans0428 <aje5@email.sc.edu>
2024-10-01 16:31:48 -0400

    finsihed assignment_8

--- csce242/assignments/assignment_8/script.js
+++ csce242/assignments/assignment_8/script.js
@@ -1,19 +1,23 @@
 const imagesArray = [
-    { src: 'images/1.png', title:'birthday' , description: 'Its his birthday!'},
-    { src: 'images/2.png', title: 'Clown', description:'Hes a clown now!'},
-    { src: 'images/3.png', title:'rain', description:'Its raining! grab an umbrella'},
-    { src: 'images/4.png', title:'read', description:'Enjoying the book!'},
-    { src: 'images/5.png', title:'shovel', description: 'Has a shovel and is ready to work'},
-    { src: 'images/6.png', title:'work', description: 'Has a laptop and is doing schoolwork'}
+    { src: './images/birthday.jpg', title:'birthday' , description: 'Its his birthday!'},
+    { src: './images/clown.jpg', title: 'Clown', description:'Hes a clown now!'},
+    { src: './images/rain.jpg', title:'rain', description:'Its raining! grab an umbrella'},
+    { src: './images/read.jpg', title:'read', description:'Enjoying the book!'},
+    { src: './images/shovel.jpg', title:'shovel', description: 'Has a shovel and is ready to work'},
+    { src: './images/work.jpg', title:'work', description: 'Has a laptop and is doing schoolwork'}
 ];
 
 const displayImages = () => {
     const container = document.getElementById('image-container');
+
+    container.innerHTML = '';
     imagesArray.forEach((image, index) => {
         const imageElement = document.createElement('img');
         imageElement.src = image.src;
         imageElement.alt = image.title;
         imageElement.classList.add('image');
+        imageElement.style.width = '150px';
+        imageElement.style.height = 'auto';
         imageElement.addEventListener('click', () => displayImageInfo(index));
         container.appendChild(imageElement);
     });
@@ -24,7 +28,7 @@ const displayImageInfo = (index) => {
     const descriptionElement = document.getElementById('image-description');
     const imageInformation = imagesArray[index];
     elementTitle.textContent = imageInformation.title;
-    descriptionElement.textContent = imageInformation;
+    descriptionElement.textContent = imageInformation.description;
 }
 
 window.onload = () => {
