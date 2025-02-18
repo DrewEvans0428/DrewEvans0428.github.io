ec7ede9984d195f6dff3e080cb2fb18550d74767
DrewEvans0428 <aje5@my.sc.edu>
2025-02-18 09:18:37 -0500

    .

---
+++ csce242-spring/excercises/04js_intro/loops.js
@@ -0,0 +1,16 @@
+document.getElementById("btn-display").onclick = () => {
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
+    const displayArea = document.getElementById("start-end-display");
+    const errorDisplay = document.getElementById("error-num-order");
+    errorDisplay.innerHTML = "";    //clear out any previous errors
+
+    if(parseInt(endNum) < parseInt(startNum)) {
+        errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
+        return; //don't do anything else if you already have an error
+    }
+
+    for(let i = startnum; i < endNum; i++){
+        console.log(i);
+    }
+}
