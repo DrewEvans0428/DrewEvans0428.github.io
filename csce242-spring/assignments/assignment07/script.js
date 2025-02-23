e1c132c3205f2df2f6d0124337a4b87c9706dc5c
DrewEvans0428 <aje5@my.sc.edu>
2025-02-23 18:00:27 -0500

    finished the climb function

--- csce242-spring/assignments/assignment07/script.js
+++ csce242-spring/assignments/assignment07/script.js
@@ -3,11 +3,13 @@ document.getElementById("draw-stairs").onclick = () => {
     let climbButton = document.getElementById("climb-stairs");
     let climber = document.getElementById("climber");
 
-
+    //Displays the ladder after draw stairs is clicked
     addLadder.style.display = "block";
+    
 
     //Used 7 to get exactly ten links since it came with 3 bars when defaulted
     for (let i=0; i < 7; i++){
+        //Portia helped me construct this for loop
         let hr = document.createElement("hr");
         hr.classList.add("tread");
         addLadder.appendChild(hr);
@@ -16,13 +18,40 @@ document.getElementById("draw-stairs").onclick = () => {
     climber.style.display = "block";
     climber.style.position = "absolute";
     //Got position numbers through trial and error
-    climber.style.top = "700px";
-    climber.style.left = "330px";
+    climber.style.bottom = "100px";
+    //climber.style.left = "auto";
+    //climber.style.margin = "auto";
+    climber.style.textAlign = "center"
     climber.src = "images/left.png";
 
+    //Displays the climb button
     climbButton.style.display = "block";
 }
 
-document.getElementById("climb-button").onclick = () => {
+document.getElementById("climb-stairs").onclick = () => {
+    let step = 0;
+    let topSteps = 5; // Got the 5 value based on the amount of steps that were taken. Used trial and error to calculate the value.
+    let climber = document.getElementById("climber");
 
+    let stepInterval = setInterval(() => {
+        if (step < topSteps) {
+            //Moves climber up 100 pixels, used parseInt to grab the value of the previous position
+            climber.style.bottom = (parseInt(climber.style.bottom) + 75) + "px";
+
+            //The modulus determines if it is an even or odd step, which determines the position
+            if (step % 2 == 0) {
+                climber.src = "images/right.png";
+            }
+            else {
+                climber.src = "images/left.png";
+            }
+            //Increments the step
+            step++;
+        }
+        else{
+            //Learned clearInterval from w3schools on JS timing page
+            clearInterval(stepInterval);
+        } 
+        //The 1000 is the interval time
+    }, 1000);
 }
