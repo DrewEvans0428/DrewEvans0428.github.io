af253d1efac2cdaf0531f548dcca6db531563bf1
DrewEvans0428 <aje5@my.sc.edu>
2025-02-24 20:09:41 -0500

    trying to get images to load

--- csce242-spring/assignments/assignment08/script.js
+++ csce242-spring/assignments/assignment08/script.js
@@ -1,20 +1,20 @@
 
     const tasks = [];
-    tasks["Crazy Clown"] = "/images/clown.jpg";
-    tasks["Happy Birthday"] = "/images/birthday.jpg";
-    tasks["It's Raining"] = "/images/rain.jpg";
-    tasks["Quiet Time"] = "/images/read.jpg";
-    tasks["Working Hard"] = "/images/shovel.jpg";
-    tasks["Work from Home"] = "/images/work.jpg";
+    tasks["Crazy Clown"] = "./images/clown.jpg";
+    tasks["Happy Birthday"] = "./images/birthday.jpg";
+    tasks["It's Raining"] = "./images/rain.jpg";
+    tasks["Quiet Time"] = "./images/read.jpg";
+    tasks["Working Hard"] = "./images/shovel.jpg";
+    tasks["Work from Home"] = "./images/work.jpg";
     const section = document.getElementById("items");
     let personImage = document.getElementById("person");
 
 
     for(let task in tasks){
-        const h4 = document.createElement("h4");
-        section.append(h4);
-        h4.innerHTML = `${task}`;
-        h4.onclick = () => {
+        const h2 = document.createElement("h2");
+        section.append(h2);
+        h2.innerHTML = `${task}`;
+        h2.onclick = () => {
             const popup = document.getElementById("popup");
             popup.style.display = "block";
             document.getElementById("popup").innerHTML = 
