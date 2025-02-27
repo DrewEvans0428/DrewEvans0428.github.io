8615d37fe8ef3729dd1424a40fdc45c5c3eca46e
DrewEvans0428 <aje5@my.sc.edu>
2025-02-27 08:33:19 -0500

    got sections working and I have the information displaying, modal still looks odd though

---
+++ csce242-spring/assignments/assignment09/script.js
@@ -0,0 +1,73 @@
+class Pizza {
+    constructor(pizzaType, toppings, sauce, cheese, price, pic){
+        this.pizzaType = pizzaType;
+        this.toppings = toppings;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.price = price;
+        this.pic = pic;
+    }
+
+    get item() {
+        const pizzaSection = document.createElement("section");
+        pizzaSection.classList.add("pizza");
+
+        const heading = document.createElement("h3");
+        heading.innerText = this.pizzaType;
+        pizzaSection.appendChild(heading);
+        pizzaSection.appendChild(this.picture(this.pic));
+
+        pizzaSection.onclick = () => {
+            this.showModal();
+        };
+        return pizzaSection;
+    }
+
+    picture(pic){
+        const picItem = document.createElement("img");
+        picItem.src = "images/" + pic;
+        return picItem;
+    }
+
+    content(pizzaType, info){
+        const p = document.createElement("p");
+        p.innerHTML = `${pizzaType}: ${info}`;
+        return p;
+    }
+
+    showModal(){
+        const modal = document.getElementById("pizza-popup");
+        const pizzaContent = document.getElementById("pizza-content");
+
+        pizzaContent.innerHTML = "";
+
+        const img = this.picture(this.pic);
+        pizzaContent.appendChild(img);
+
+        pizzaContent.append(this.content("Toppings", this.toppings));
+        pizzaContent.append(this.content("Sauce ", this.sauce));
+        pizzaContent.append(this.content("Cheese ", this.cheese));
+        pizzaContent.append(this.content("Price ", this.price));
+
+        modal.style.display = "block";
+
+
+    }
+
+   
+}
+
+window.onload = () => {
+    let pizzas = [];
+    let pizzaList = document.getElementById("pizzas-list");
+
+    pizzas.push(new Pizza("Pepperoni", "pepperoni, basil", "Tomato/Marinara", "Mozzarella", "$8", "pep.jpg"));
+    pizzas.push(new Pizza("Hawaiian", "Pineapple, Ham, Jalapenos", "Tomato Sauce", "Mozzarella", "$20.99", "hawaiian.jpg"));
+    pizzas.push(new Pizza("Deluxe", "Pepperoni, Onions, Mushrooms, Peppers", "Tomato Sauce", "Provolone, Mozzarella", "$20.99", "deluxe.jpg"));
+    pizzas.push(new Pizza("Spinach & Feta", "Onions, Spinach", "Alfredo Sauce", "Provolone, Feta, Asiago", "$20.99", "spin.jpg"));
+    pizzas.push(new Pizza("Pacific Veggie", "Onions, Diced Tomatoes, Black Olives, Spinach, Mushrooms", "Tomato Sauce", "Provolone, Feta", "$20.99", "veggie.jpg"));
+
+    for (let i in pizzas) {
+        pizzaList.append(pizzas[i].item);
+    }
+};
